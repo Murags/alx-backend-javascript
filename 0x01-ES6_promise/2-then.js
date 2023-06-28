6446811eb73a337c574a497b8825803d8156b65b
Dennis Murage <dmurage140@gmail.com>
2023-06-28 17:20:45 +0300

    fixing return statement

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -5,7 +5,7 @@ export default function handleResponseFromAPI(promise) {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch((error) => {
+    .catch((error = new Error()) => {
     /* eslint-enable */
       console.log('Got a response from the API');
       return error;
