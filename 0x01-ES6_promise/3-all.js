cf5322abc90dc08d839770d0579d47d7f7c6878a
Dennis Murage <dmurage140@gmail.com>
2023-06-28 18:16:17 +0300

    task 3 files

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,14 @@
 import { createUser, uploadPhoto } from './utilis';
 
-export default function handleProfileSignup() {
-  uploadPhoto()
-    .then((data) => {
-      createUser()
-        .then((userdata) => {
-          console.log(`${data.body} ${userdata.firstName} ${userdata.lastName}`);
-        })
-        .catch((error = new Error('Signup system offline')) => console.log(error));
-    })
-    .catch((error = new Error('Signup system offline')) => console.log(error));
+export default async function handleProfileSignup() {
+  try {
+        const data = await uploadPhoto();
+        createUser()
+            .then((userdata) => {
+                console.log(`${data.body} ${userdata.firstName} ${userdata.lastName}`);
+            })
+            .catch((error = new Error('Signup system offline')) => console.log(error));
+    } catch (error_1) {
+        return console.log(error_1);
+    }
 }
