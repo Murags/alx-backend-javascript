c89babd1795dfa34dadb97f3d1d8c6f67bfe4f94
Dennis Murage <dmurage140@gmail.com>
2023-06-28 17:26:10 +0300

    fixing return statement

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then((data = { status: 200, body: 'success' }) => {
-      return data;
-    })
-    .catch((error = new Error()) => {
-      return error;
-    })
-    .finally(console.log('Got a response from the API')
-    )};
+  return promise
+    .then((data = { status: 200, body: 'success' }) => data)
+    .catch((error = new Error()) => error)
+    .finally(console.log('Got a response from the API'));
+}
