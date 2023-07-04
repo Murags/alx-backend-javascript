a7039e28a072b6c2a7c097768299ad923356d5dc
Dennis Murage <dmurage140@gmail.com>
2023-07-04 10:50:31 +0300

    Error Handle

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,12 @@
 export default function updateUniqueItems(mapObject) {
-  for (const [key, value] of mapObject) {
-    if (value === 1) {
-      try {
+  if ((mapObject instanceof Map)) {
+    for (const [key, value] of mapObject) {
+      if (value === 1) {
         mapObject.set(key, 100);
-      } catch (e) {
-        throw new Error('Cannot process');
       }
     }
+  } else {
+    throw new Error('Cannot process');
   }
   return mapObject;
 }
