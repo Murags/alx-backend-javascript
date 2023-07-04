0ab0796249c6a0f0e5bf5bd9d65175876ab0af58
Dennis Murage <dmurage140@gmail.com>
2023-07-04 10:54:50 +0300

    -F

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, str) {
-  if (str && typeof str === "string") {
+  if (str && typeof str === 'string') {
     const arr = [...set];
     return arr.filter((item) => item.startsWith(str)).map((item) => item.slice(3)).join('-');
   }
