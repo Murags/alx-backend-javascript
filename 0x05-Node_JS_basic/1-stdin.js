a1d565879922eb924b10ef27e38a9b9a80b184de
Dennis Murage <dmurage140@gmail.com>
2023-08-22 17:46:35 +0300

    Refactor to close after execution

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,13 @@
 const readline = require('readline');
 
-const inter = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
 console.log('Welcome to Holberton School, what is your name?');
-
-inter.on('line', (line) => {
-  const name = line.toString();
+rl.on('line', (name) => {
   console.log(`Your name is: ${name}`);
-  process.exit();
-});
-
-inter.on('close', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
+  rl.close();
 });
